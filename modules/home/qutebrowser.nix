d37fd9205477e60f263577d7b27974ed512bb8b7
Gordon <gordonchen2014@gmail.com>
2025-01-05 17:21:12 -0500

    nixify qutebrowser configs

---
+++ modules/home/qutebrowser.nix
@@ -0,0 +1,36 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.qutebrowser = {
+    enable = true;
+    searchEngines = {
+      youtube = "https://www.youtube.com/results?search_query={}";
+      wikipedia = "https://en.wikipedia.org/wiki/Special:Search/{}";
+      math-stackexchange = "https://math.stackexchange.com/search?q={}";
+      reddit = "https://www.reddit.com/search/?q={}";
+      github = "https://github.com/search?q={}";
+      google = "https://www.google.com/q?={}";
+
+      # On the chopping block
+      archwiki = "https://wiki.archlinux.org/title/Special:Search/{}";
+      archpackages = "https://archlinux.org/packages/?q={}";
+      nixpkgs = "https://search.nixos.org/packages?query={}";
+      nixos = "https://search.nixos.org/options?query={}";
+    };
+    keyBindings = {
+      normal = {
+        "<Ctrl-o>" = "tab-focus stack-prev";
+        "<Ctrl-i>" = "tab-focus stack-next";
+
+        "ey" = ":cmd-set-text :tab-focus https://www.youtube.com";
+        "eq" = ":cmd-set-text :tab-focus https://q.utoronto.ca";
+      };
+    };
+    extraConfig = ''
+      # config.source('/home/gordo/nix-cfg/qutebrowser/config.py')
+    '';
+  };
+}
